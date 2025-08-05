07bfa71f88a42cc671aaae4a953fd963c04b839f
Ben Hansen <Benjamin.t.hansen@icloud.com>
2025-08-05 19:35:41 +0000

    finished

---
+++ Reflection_Final.py
@@ -0,0 +1,32 @@
+"""
+
+The grade I would propose for the course is an A-B maybe around 87-92. 
+My reasoning for this starts with attendance and punctuality. I thought I did a very good job joining the meetings on time usually around 11:25-11:28 to be there 
+on time and have my notes and code spaces up and ready to go. I think that I was extremely present by taking notes in my note book and really trying to understand 
+the topics and syntax’s talk about. As well I would code alongside the professor on my own code editor.  Trying to manipulate the code talked about in class to better 
+understand the operators and logical approaches that are needed to solve some of the homework assignments. For attendance I only missed 1 session which was 7/7/25 due
+ to travel. For participation and engagement I thought I did a good job. I asked a handful of questions and answered a handful throughout the course. I feel like I did 
+ have a lot of engagement throughout the course really taking note of the reading we had assigned. Also challenging myself in further problems on my own, once I noticed 
+ near the end of the course the professor starting giving us examples of logical thinking in the data structure and algorithm approaches I started to do some easier 
+ leetcode problems. These 2 problems were the (Two Sum) and (Roman to Integer) although I wasn’t able to solve them I used the logical thinking that I learned in this 
+ course to approach them and write out how I would solve these problems. Then I would attempt to develop a method with the skills I learned in this course. For Presence 
+ and Focus I was very mentally present and was really able to understand the topics talked about from the professor's methods of teaching. Especially drawing the problem
+ out really puts into perspective of how the professor would approach things. I was free from distractions taking all calls in my room that has 2 monitors with one having
+   the class call on 1 and the other having a separate code editor to follow along with the professors code examples. Most of the help I needed was found in the reading 
+   so there was no real need at least for me to attend office hours. For assignment timeliness all coding assignments were submitted on time and synced with code spaces,
+     as I did not miss a single assignment. For reflection and growth I thought I did a great job with reviewing the differences in the code as well as try to learn how
+       the professors wanted methods to look. Each time I would review the solutions to the weeks previous I would at least try to pick up one thing differently and
+         include it into my code. The solutions would allow me to benefit in matching up with future solutions and help me develop a proper way to code. For code quality 
+         I thought I did good, in the start I definitely didn’t have the best looking code but throughout the course I really tried to improve. Looking back at my first
+           few assignments I definitely improved and now have a different style of coding. Adding comments to my code was definitely a struggle to add as it was a feature
+             I forgot to add a few times, this is why I have marked myself down for a grade. Looking forward it's mandatory to include for future courses but also the
+               corporate would and coding industry. It is very necessary for people to understand your code and also your logic behind code. Throughout this course I feel
+                 like I have really learned a lot. Growing up I learned python and have always coded in it. However this course taught me way more than just python. It has 
+                 given me the fundamentals to be able to learn other languages that are similar. It has also given me a more professional look at how coding should be 
+                 structured and interpreted. I overcame a lot of challenges, one of the hardest for me was understanding how object orientation really worked. I’ve known 
+                 simple python loops,variables and some operators but this course really pushed me to understand a lot more about what this language can do.
+
+
+
+
+"""
---
+++ Reflection_last_HW.py
@@ -0,0 +1,13 @@
+"""
+Looking at the solution at week 9 I can see some differences in the code. For the most part I was able to get most of the solutions correct. 
+In the sum of digits I didn’t include in the beginning to check if the number was a base of 10. Although I didn't include this, the code does run and has the 
+same functionality. The count of occurrences had some different logic as I used a different counting method that keeps the data in sync with the recursion. 
+I did mess up the first factorial as it is not recursive and is not the method that was looked for as my math and logic was messed up. Although the other fibonacci 
+was right and does match up with the solution. This week was harder than most for me as the recursion was a little confusing. But after working on these problems I 
+understand how to approach these problems for the future.
+
+
+
+
+
+"""
